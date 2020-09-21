49926ded211db84cd9c228f4504e3ca4cec50979
hiroygo <pcxn1a@gmail.com>
2020-09-22 01:04:46 +0900

    update

--- main.cpp
+++ main.cpp
@@ -6,20 +6,18 @@
 
 int ChildDo(std::shared_ptr<int> ptr)
 {
-    const auto pid = getpid();
-    printf("i am a child, pid: %d, ptr:%p\n", pid, ptr.get());
+    printf("i am a  child, pid: %d, testPtr:%p\n", getpid(), ptr.get());
     sleep(10);
     return 0;
 }
 
 int ParentDo(const int childPid, std::shared_ptr<int> ptr)
 {
-    const auto pid = getpid();
-    printf("i am a parent, pid: %d, ptr:%p\n", pid, ptr.get());
+    printf("i am a parent, pid: %d, testPtr:%p\n", getpid(), ptr.get());
 
-    int status = 0;
-    // wait(&status) は waitpid(-1, &status, 0) と等価
-    const auto waitRet = wait(&status);
+    int childStatus = 0;
+    // wait(&childStatus) は waitpid(-1, &childStatus, 0) と等価
+    const auto waitRet = wait(&childStatus);
     if (waitRet == -1)
     {
         perror("wait error\n");
@@ -33,13 +31,13 @@ int ParentDo(const int childPid, std::shared_ptr<int> ptr)
         return 1;
     }
 
-    if (WIFEXITED(status))
+    if (WIFEXITED(childStatus))
     {
-        printf("子プロセスが正常終了しました %d\n", WEXITSTATUS(status));
+        printf("子プロセスが正常終了しました %d\n", WEXITSTATUS(childStatus));
     }
-    if (WIFSIGNALED(status))
+    if (WIFSIGNALED(childStatus))
     {
-        printf("子プロセスがシグナルにより終了しました %d\n", WTERMSIG(status));
+        printf("子プロセスがシグナルにより終了しました %d\n", WTERMSIG(childStatus));
     }
 
     return 0;
@@ -47,7 +45,12 @@ int ParentDo(const int childPid, std::shared_ptr<int> ptr)
 
 int main()
 {
-    printf("プロセスの開始\n");
+    printf("プロセス開始\n");
+    auto pTest = std::make_shared<int>(100);
+
+    // fork() は呼び出し元プロセスを複製して新しいプロセスを生成する
+    // child プロセスは、基本的に parent プロセスの複製 -> 変数の値やポインタのアドレスも同じ
+    // 複製できない部分もあるので詳細は `man fork` で確認すること
     const auto pid = fork();
     if (pid == -1)
     {
@@ -55,11 +58,11 @@ int main()
         return 1;
     }
 
-    auto ptr = std::make_shared<int>(1);
+    // ここから child プロセスの動作が開始する
     if (pid == 0)
     {
-        return ChildDo(ptr);
+        return ChildDo(pTest);
     }
 
-    return ParentDo(pid, ptr);
+    return ParentDo(pid, pTest);
 }
